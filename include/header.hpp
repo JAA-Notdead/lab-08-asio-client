a665d65059d8401819f8c164c13276ca5622fc10
berkut126 <berkut126@gmail.com>
2020-02-23 18:29:25 +0300

    Added BOOST to CMakeLists.txt. Fixed warnings in .travis.yml. Initial push.

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,8 @@
 // Copyright 2018 Your Name <your_email>
-
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
-
-#endif // INCLUDE_HEADER_HPP_
+#pragma once
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
+using boost::system::error_code;
